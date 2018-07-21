882bbfbaec78fe023faaaba8495d37f5db868f1c
Jessica Howard <jessicahoward1031@gmail.com>
2018-07-21 12:31:49 -0400

    jquery variables and methods started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,19 @@
     
       
     $(document).ready(function() { 
+        //array of numbers 1-12 to be assiigned to each crystal at beggining of game. this number is hidden from the user
+    var crystalNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
         // empty variables
     var wins ;
     var losses ;
-    var crystal1 ;
-    var crystal2 ;
-    var crystal3 ;
-    var crystal4 ;
+    var crystal1 = Math.floor(Math.random() * crystalNumbers.length) +1;
+    // alert(crystal1);
+    var crystal2 = Math.floor(Math.random() * crystalNumbers.length) +1;
+    // alert(crystal2);
+    var crystal3 = Math.floor(Math.random() * crystalNumbers.length) +1;
+    // alert(crystal3);
+    var crystal4 = Math.floor(Math.random() * crystalNumbers.length) +1;
+    // alert(crystal4);
     var scoreCounter ;
     //array of numbers 1-12 to be assiigned to each crystal at beggining of game. this number is hidden from the user
     var crystalNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
@@ -23,9 +29,29 @@
         // console.log(target);
         $("#target").text(target);
 
-
+        
 //button digit assignments are hidden form user
 //when button is clicked, associated number is displayed in user score display
+
+$(".crystal1").on("click", function (){
+    crystal1 = parseInt(crystal1);
+    $("#score").text(crystal1);
+})
+
+$(".crystal2").on("click", function (){
+    crystal2 = parseInt(crystal2);
+    $("#score").text(crystal2);
+})
+
+$(".crystal3").on("click", function (){
+    crystal3 = parseInt(crystal3);
+    $("#score").text(crystal3);
+})
+
+$(".crystal4").on("click", function (){
+    crystal4 = parseInt(crystal4);
+    $("#score").text(crystal1);
+})
 //with each additional button click, associated number is added to 
 //user score and total is displayed
 //when user score matches target score
